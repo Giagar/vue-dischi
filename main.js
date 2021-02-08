f5daa712024a35c8fa9cedaecbe73138b5db45b5
Gianpaolo <pao.barga@gmail.com>
2021-02-08 13:10:49 +0100

    esercizio base fatto e impostata select dinamica

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-$(document).ready(function() {
-	//Code
-});
+new Vue({
+	el: "#root",
+	data: {
+		list: [],
+		genre: "",
+	},
+	mounted() {
+		axios
+		.get("https://flynn.boolean.careers/exercises/api/array/music")
+		.then(res => this.list = res.data.response)
+		console.log("list", this.list);
+	},
+
+	methods: {
+	},
+})
