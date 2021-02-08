8a9422ffc039b7fc122aa5cd3ba22892497edd23
Gianpaolo <pao.barga@gmail.com>
2021-02-08 16:12:27 +0100

    collegato rendering album a select dinamica (cambiato i dati di riferimento per option e lista album)

--- main.js
+++ main.js
@@ -2,15 +2,35 @@ new Vue({
 	el: "#root",
 	data: {
 		list: [],
+		optionList: [],
+		albumList: [],
 		genre: "",
 	},
 	mounted() {
 		axios
 		.get("https://flynn.boolean.careers/exercises/api/array/music")
-		.then(res => this.list = res.data.response)
-		console.log("list", this.list);
+		.then(res => {
+			res = res.data.response;
+			// storing data
+			this.list = res;
+
+			// creating a copy of data which I will modify everytime I select an option
+			this.albumList = res;
+
+			// creating an immutable list of options without duplicates
+			res.forEach(album => {
+				this.optionList.push(album.genre)
+			})
+
+		})
 	},
 
 	methods: {
+		
+		handleSelection() {
+			
+			this.albumList = this.list.filter(album => album.genre === this.genre);
+			console.log("ok");
+		}
 	},
 })
